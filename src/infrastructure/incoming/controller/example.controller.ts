d43977a7de46db6ffc2df5018c55b7d56c53610f
Bosco Domingo <boscodomingob@gmail.com>
2023-06-20 01:33:22 +0200

    Refactor example controller

--- src/infrastructure/incoming/controller/example.controller.ts
+++ src/infrastructure/incoming/controller/example.controller.ts
@@ -1,25 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { randomUUID } from 'crypto';
 
 import { Request, Response } from 'express';
 
-import { CreateUser } from '../../../application/domain1/ExampleUseCase.js';
-import { CreateUserUseCaseOutput } from '../../../application/domain1/ExampleUseCase.output.js';
+import { CreateUserUseCase } from '../../../application/domain1/createUser/createUserUseCase.js';
+import { CreateUserUseCaseOutput } from '../../../application/domain1/createUser/createUserUseCase.output.js';
+import { ExampleError } from '../../../domain/domain1/example.error.js';
 import { User } from '../../../domain/domain1/user.entity.js';
 import { LocalDBUserRepository } from '../../outgoing/database/localDBUserRepository.adapter.js';
 
-const useCaseExample: CreateUser = new CreateUser(new LocalDBUserRepository());
+const useCaseExample: CreateUserUseCase = new CreateUserUseCase(new LocalDBUserRepository());
 
-export class ControllerExample {
+export class ExampleController {
 	public createUser = async (req: Request, res: Response): Promise<void> => {
 		try {
 			const result: CreateUserUseCaseOutput = await useCaseExample.execute({
 				user: new User(randomUUID(), req.body.id, new Date(), new Date()),
 			});
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 			res.status(200).json(result);
 		} catch (error) {
 			// log error
 			console.error(error as Error);
+			throw new ExampleError();
+		}
+	};
+
+	public findUser = async (req: Request, res: Response): Promise<void> => {
+		try {
+			const result: User = await new LocalDBUserRepository().findByCriteria();
+			res.status(200).json(result);
+		} catch (error) {
+			// log error
+			console.error(error as Error);
+			throw new ExampleError();
 		}
 	};
 }
